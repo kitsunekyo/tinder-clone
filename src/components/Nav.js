46a927224151b8c9b26eff7c5684c5b438dd4907
Alex Spieslechner <alexander.spieslechner@gmail.com>
2022-03-12 19:18:59 +0100

    set logo as navlink

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,19 @@
 import logoWhite from "../images/logo-white.svg";
 import logoDark from "../images/logo-full.svg";
+import { NavLink } from "react-router-dom";
 
 export const Nav = ({ minimal, setShowModal, setIsSignUp }) => {
   const authToken = true;
 
   return (
     <nav className="flex items-center p-6">
-      <div>
+      <NavLink to="/">
         <img
           src={minimal ? logoDark : logoWhite}
           alt="tinder"
           className="h-8"
         />
-      </div>
+      </NavLink>
       {!authToken && !minimal && (
         <div className="ml-auto">
           <button
